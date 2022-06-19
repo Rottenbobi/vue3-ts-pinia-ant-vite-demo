030f78b633d2bff7082cf3ca0f205b740bc7eadc
Cat ballad <100394119+Rottenbobi@users.noreply.github.com>
2022-06-19 10:44:40 +0800

    基础布局

--- src/views/clues/clue/index.vue
+++ src/views/clues/clue/index.vue
@@ -1,10 +1,14 @@
 <template>
- 123
+  <a-tabs v-model:activeKey="activeKey">
+    <a-tab-pane key="1" tab="线索">Content of Tab Pane 1</a-tab-pane>
+    <a-tab-pane key="2" tab="线索池" force-render>Content of Tab Pane 2</a-tab-pane>
+
+  </a-tabs>
 </template>
  
 <script lang='ts' setup>
 import { ref,reactive } from 'vue'
-
+let activeKey = ref('1')
 </script>
  
 <style scoped>
